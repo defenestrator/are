2b54e0498fa1a99c49ff18fd745b6586db195704
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2025-01-21 08:18:37 -0700

    investments

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return ['equity' => "The investment we made in ourselves, version " . app()->version()];
 });
 
 require __DIR__.'/auth.php';
