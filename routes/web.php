025dfaa6f6b335dcf73c543c58ff299cb138bb3e
Taylor Otwell <taylor@laravel.com>
2025-02-26 10:06:59 -0700

    wip

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,17 @@ Route::get('/', function () {
 
 Route::get('vote', function() {
     return view('vote', []);
+
 })
     ->middleware(['auth'])
     ->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
-    // Route::redirect('settings', 'settings/profile');
+    Route::redirect('settings', 'settings/profile');
 
-    // Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    // Volt::route('settings/password', 'settings.password')->name('settings.password');
-    // Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
+    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
+    Volt::route('settings/password', 'settings.password')->name('settings.password');
+    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
