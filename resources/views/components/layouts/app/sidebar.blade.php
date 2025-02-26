ab2c5f136aa97b43e616fc84789a2c334cc32ae2
Taylor Otwell <taylor@laravel.com>
2025-02-26 10:39:04 -0700

    wip

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -13,7 +13,7 @@
 
             <flux:navlist variant="outline">
                 <flux:navlist.group heading="Platform" class="grid">
-                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+                    <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>Vote</flux:navlist.item>
                 </flux:navlist.group>
             </flux:navlist>
 
