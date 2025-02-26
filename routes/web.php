0750d228cc61e2fb0e9c90ec8e29c48c396c72c9
Prime Again <the.primeagain@gmail.com>
2025-02-26 10:17:34 -0700

    feat: model

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     if (Auth::check()) {
@@ -19,10 +18,6 @@ Route::get('vote', function() {
 
 Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
 });
 
 require __DIR__.'/auth.php';
