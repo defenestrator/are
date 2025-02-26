360b676b2bb050ee71adb36043dd9964214135aa
Taylor Otwell <taylor@laravel.com>
2025-02-26 08:47:51 -0700

    wip

--- routes/auth.php
+++ routes/auth.php
@@ -7,9 +7,10 @@ Route::middleware('guest')->group(function () {
     Route::get("login", function() {
         return Socialite::driver("twitch")->redirect();
     });
+
     Route::get("twitch/auth", function() {
         $user = Socialite::driver("twitch")->user();
-        dd($user);
+        dd($user->name);
     });
 });
 
