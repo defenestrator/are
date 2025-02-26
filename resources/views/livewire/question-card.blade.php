2cb68ed59f8e7aca6b61a6e9405dd9fbe0fc0e59
Taylor Otwell <taylor@laravel.com>
2025-02-26 12:53:23 -0700

    wip

--- resources/views/livewire/question-card.blade.php
+++ resources/views/livewire/question-card.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Question;
+use Flux\Flux;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 use Livewire\Volt\Component;
@@ -8,6 +9,7 @@ use Livewire\Volt\Component;
 new class extends Component {
     public Question $question;
     public int $voteCount;
+    public string $questionBeingEdited;
 
     public function upvote(Question $question) {
         DB::table('question_votes')->updateOrInsert([
@@ -29,6 +31,18 @@ new class extends Component {
         ]);
 
         $this->voteCount = $this->question->voteCount();
+    }
+
+    public function updateQuestion()
+    {
+        if ($this->question->user_id !== Auth::user()->id) {
+            return;
+        }
+
+        $this->question->question = $this->questionBeingEdited;
+        $this->question->save();
+
+        Flux::modals()->close();
     }
 } ?>
 
@@ -66,12 +80,30 @@ new class extends Component {
 
                 <flux:menu class="min-w-0">
                     @if (Auth::user()->id === $question->user_id)
-                        <flux:menu.item icon="pencil-square">Edit</flux:menu.item>
+                        <flux:modal.trigger name="edit-question">
+                            <flux:menu.item icon="pencil-square">Edit</flux:menu.item>
+                        </flux:modal.trigger>
                     @endif
 
                     <flux:menu.item variant="danger" icon="trash">Delete</flux:menu.item>
                 </flux:menu>
             </flux:dropdown>
+
+            <flux:modal name="edit-question" class="md:w-96">
+                <div class="space-y-6">
+                    <div>
+                        <flux:heading size="lg">Edit Question</flux:heading>
+                    </div>
+
+                    <flux:input label="Question" wire:model="questionBeingEdited" :value="$question->question" />
+
+                    <div class="flex">
+                        <flux:spacer />
+
+                        <flux:button type="submit" variant="primary" wire:click="updateQuestion">Save</flux:button>
+                    </div>
+                </div>
+            </flux:modal>
         </div>
     </div>
 </div>
