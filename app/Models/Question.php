7925a34608d2b78913a734016da88093add3580b
TJ DeVries <devries.timothyj@gmail.com>
2025-02-26 14:36:20 -0700

    tmp: move limit to five questions

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,19 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Question extends Model {
-    protected $guarded = [ ];
-    public function user() {
+class Question extends Model
+{
+    protected $guarded = [];
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public static function getSortedQuestions() {
+    public static function getSortedQuestions()
+    {
         return self::query()
             ->leftJoin('question_votes', 'questions.id', '=', 'question_votes.question_id')
             ->selectRaw('questions.*, coalesce(sum(question_votes.count), 0) as votes')
             ->orderBy('votes', 'desc')
             ->groupBy('questions.id')
-            ->limit(1000)
+            ->limit(5)
             ->with('user')
             ->get();
     }
