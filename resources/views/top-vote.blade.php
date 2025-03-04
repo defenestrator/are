2a8a1eabc1eb8c6380ff21845f2e16ebc2f2a38b
Prime Again <the.primeagain@gmail.com>
2025-03-04 11:25:10 -0700

    obs: ... i hate you

--- resources/views/top-vote.blade.php
+++ resources/views/top-vote.blade.php
@@ -18,24 +18,24 @@ new class extends Component {
     </head>
     <body>
     @volt
-        <flux:card style="background-color: #fafafa" class="m-2 rounded-lg max-w-120 bg-zinc-400/5 dark:bg-zinc-900">
+        <flux:card style="background-color: #fafafa" class="text-xl m-2 rounded-lg bg-zinc-400/5 dark:bg-zinc-900">
             <div class="pl-2">
-                <flux:text class="mb-2" variant="strong">{{ $question->question }}</flux:text>
+                <flux:text class="mb-2 text-xl" variant="strong">{{ $question->question }}</flux:text>
 
                 <div class="flex jusify-between items-center">
                     <div class="flex items-center mr-auto">
-                        <flux:text class="w-4 max-w-4 min-w-4 text-sm mr-2 text-zinc-500 dark:text-zinc-400 tabular-nums">
+                        <flux:text class="text-xl w-4 max-w-4 min-w-4 mr-2 text-zinc-500 dark:text-zinc-400 tabular-nums">
                             {{ $question->votes }}</flux:text>
 
                         <div class="flex items-center gap-2">
                             <flux:icon.hand-thumb-up name="hand-thumb-up" variant="outline"
-                                class="size-4 text-zinc-400 [&_path]:stroke-[2.25]" />
+                                class="size-5 text-zinc-400 [&_path]:stroke-[2.25]" />
                         </div>
                     </div>
 
                     <div class="flex items-center pt-2 gap-2">
-                        <flux:avatar src="{{ $question->user->twitch_avatar_url }}" size="xs" class="shrink-0" />
-                        <flux:subheading variant="strong">
+                        <flux:avatar src="{{ $question->user->twitch_avatar_url }}" class="shrink-0" />
+                        <flux:subheading class="text-xl" variant="strong">
                             {{ $question->user->name }}
                         </flux:subheading>
                     </div>
