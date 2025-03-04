8e0cbe5646b3e7ffb3d8a25b6efb76e6e41a2126
Prime Again <the.primeagain@gmail.com>
2025-03-04 11:14:59 -0700

    feat: top-blade now just renders the question directly

--- resources/views/top-vote.blade.php
+++ resources/views/top-vote.blade.php
@@ -18,7 +18,31 @@ new class extends Component {
     </head>
     <body>
     @volt
-        <livewire:question-card :question="$question" :vote-count="$question->votes"/>
+        <flux:card class="m-2 rounded-lg max-w-120 bg-zinc-400/5 dark:bg-zinc-900">
+            <div class="pl-2">
+                <flux:text class="mb-2" variant="strong">{{ $question->question }}</flux:text>
+
+                <div class="flex jusify-between items-center">
+                    <div class="flex items-center mr-auto">
+                        <flux:text class="w-4 max-w-4 min-w-4 text-sm mr-2 text-zinc-500 dark:text-zinc-400 tabular-nums">
+                            {{ $question->votes }}</flux:text>
+
+                        <div class="flex items-center gap-2">
+                            <flux:icon.hand-thumb-up name="hand-thumb-up" variant="outline"
+                                class="size-4 text-zinc-400 [&_path]:stroke-[2.25]" />
+                        </div>
+                    </div>
+
+                    <div class="flex items-center pt-2 gap-2">
+                        <flux:avatar src="{{ $question->user->twitch_avatar_url }}" size="xs" class="shrink-0" />
+                        <flux:subheading variant="strong">
+                            {{ $question->user->name }}
+                        </flux:subheading>
+                    </div>
+
+                </div>
+            </div>
+        </flux:card>
     @endvolt
     </body>
 </html>
