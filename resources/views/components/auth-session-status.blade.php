4b44de1f13cc6b20be8e868b2966385f974e6172
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2025-03-06 11:16:16 -0700

    more merge madness because dumb

--- resources/views/components/auth-session-status.blade.php
+++ resources/views/components/auth-session-status.blade.php
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-@props(['status'])
-
-@if ($status)
-    <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600 dark:text-green-400']) }}>
-=======
 @props([
     'status',
 ])
 
 @if ($status)
     <div {{ $attributes->merge(['class' => 'font-medium text-sm text-green-600']) }}>
->>>>>>> origin/master
         {{ $status }}
     </div>
 @endif
