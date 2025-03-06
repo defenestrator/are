b181a15ed4b213c0e94047302654214dd647b143
Jeremy Anderson <29446+defenestrator@users.noreply.github.com>
2025-03-06 11:41:20 -0700

    renameroute

--- routes/auth.php
+++ routes/auth.php
@@ -57,5 +57,5 @@ Route::middleware('guest')->group(function () {
 Route::post('logout', App\Livewire\Actions\Logout::class)
     ->name('logout');
 
-    Route::get('logout', App\Livewire\Actions\Logout::class)
-    ->name('logout');
+Route::get('logout', App\Livewire\Actions\Logout::class)
+    ->name('getLogout');
